4ac1cc512e7097c2e08d192e81353a3b53bf567c
GodishalaAshwith <ashwith7777@gmail.com>
2025-06-02 23:03:31 +0530

    UI_v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 import UploadPage from "./Pages/UploadPage";
 import EditorPage from "./Pages/EditorPage";
 import Layout from "./components/Layout";
@@ -13,7 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<UploadPage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/upload" element={<UploadPage />} />
             <Route path="/editor" element={<EditorPage />} />
           </Route>
